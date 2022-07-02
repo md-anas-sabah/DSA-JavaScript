98b9b54b4d60a79488e536e3a0457488a4263979
Md Anas Sabah <mdanassabah@gmail.com>
2022-07-02 21:40:44 +0530

    first commit

---
+++ Math Algorithm/Prime.js
@@ -0,0 +1,11 @@
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+console.log(isPrime(7)); // true
+console.log(isPrime(8)); //false
